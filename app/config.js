f0fa5f04a0b4cdb34a766fae761f8c002b00f1f2
Rachel Derbyshire <rachel.derbyshire@engineering.digital.dwp.gov.uk>
2024-09-26 13:53:09 +0100

    hide change link on the print screen

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Access to work - Apply (agent sandbox) ',
+  serviceName: 'Access to work - Apply ',
 
   // Default port that prototype runs on
   port: '3000',
